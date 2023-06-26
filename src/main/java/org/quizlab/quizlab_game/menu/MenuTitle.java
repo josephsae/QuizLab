07ba810cfe29861f325db1abbc579249f4925b3c
josephsae <joseph.caice08@gmail.com>
2023-06-26 13:55:54 -0500

    Migrate project to maven

---
+++ src/main/java/org/quizlab/quizlab_game/menu/MenuTitle.java
@@ -0,0 +1,43 @@
+package org.quizlab.quizlab_game.menu;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+/**
+ * Esta clase representa el título de un menú
+ * 
+ *  @see Pane
+ */
+public class MenuTitle extends Pane {
+	
+	private Font titleFont = Font.loadFont(getClass().getResourceAsStream("/org/quizlab/quizlab_game/Iceland-Regular.ttf"), 150);
+	private Text titleText;
+/**
+ * Crea un objeto título para el menú con el nombre
+ * @param name Nombre del título
+ */
+	public MenuTitle(String name) {
+		titleText = new Text(name);
+		
+		titleText.setFont(titleFont);
+		titleText.setFill(Color.WHITE);
+
+		getChildren().addAll(titleText);
+	}
+/**
+ * Obtiene el ancho del título de un menú
+ * @return El ancho del título
+ */
+	public double getTitleWidth() {
+		return titleText.getLayoutBounds().getWidth();
+	}
+/**
+ * Obtiene la altura del título de un menú
+ * @return La altura del título
+ */
+	public double getTitleHeight() {
+		return titleText.getLayoutBounds().getHeight();
+	}
+}
---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.quizlab.quizlab_game","l":"org.quizlab.quizlab_game"},{"m":"org.quizlab.quizlab_game","l":"org.quizlab.quizlab_game.game"},{"m":"org.quizlab.quizlab_game","l":"org.quizlab.quizlab_game.levels"},{"m":"org.quizlab.quizlab_game","l":"org.quizlab.quizlab_game.menu"}];updateSearchResults();
